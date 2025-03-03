9f85561b8526eda560f55bee1aaaa3cda7521675
Boru Isako <isco.jaldy@gmail.com>
2025-03-03 16:13:27 +0300

    Complete labs

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random()*10) +1
+
+
+const num3 = 18;
+const num4 = 7;
+const mod = num3%num4
+console.log(mod)
+
+const max = Math.max(0,5,10,15,20)
